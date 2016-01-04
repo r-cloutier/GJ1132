aa1f42af4596b568707343978c560d1e948f41d5
Ryan Cloutier <cloutier@astro.utoronto.ca>
2016-01-04 13:55:39 -0500

    slightly modified imports.py

--- imports.py
+++ imports.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pylab as plt
-import triangle
+import corner
 import scipy.optimize as op
 import sys
 import os
@@ -11,8 +11,8 @@ import cPickle as pickle
 from PyAstronomy.pyasl import foldAt
 import glob
 import time
-import rvs
-import compute_hz as chz
+#import rvs
+#import compute_hz as chz
 from scipy.stats import ks_2samp
 from scipy.interpolate import interp1d
 from matplotlib.colors import LogNorm
